bd87031649aa90efcb9ea994d8f539d80b68dd1c
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2022-11-18 16:00:37 +0700

    for page not found set up

---
+++ frontend/src/pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,16 @@
+import {memo} from 'react'
+import {Container} from "react-bootstrap"
+
+function PageNotFound() {
+    return (
+        <>
+        <Container>
+            
+        </Container>
+        404 Page Not Found
+        Sorry please go back to the home page.
+        </>
+    )
+}
+
+export default memo(PageNotFound)
