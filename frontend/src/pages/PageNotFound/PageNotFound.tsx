9ffd46c8fd63e5966240c0d98fa4fcd7ecea0a64
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2022-11-19 14:55:51 +0700

    adjust for page not found

--- frontend/src/pages/PageNotFound/PageNotFound.tsx
+++ frontend/src/pages/PageNotFound/PageNotFound.tsx
@@ -1,14 +1,24 @@
 import {memo} from 'react'
-import {Container} from "react-bootstrap"
+import {Container, Row, Col} from "react-bootstrap"
+import styles from "./PageNotFound.module.css"
+import Brand from "../../components/Brand/Brand"
 
 function PageNotFound() {
     return (
         <>
-        <Container>
+        <Container fluid className={styles.container}>
+            <Row className={styles.font}>
+                    404 Page Not Found  
+                <Col className="w-100 d-flex align-items-center mt-3" style={{opacity:0.4}}>
+                <Brand logo_content={styles.logo_content} logo_style={styles.logo}></Brand>
+                </Col>
+            </Row>
+            <Row className={styles.font}>
+                <div className={styles.seperate}></div>
+                    Sorry please go back to the home page.   
+            </Row>
             
         </Container>
-        404 Page Not Found
-        Sorry please go back to the home page.
         </>
     )
 }
