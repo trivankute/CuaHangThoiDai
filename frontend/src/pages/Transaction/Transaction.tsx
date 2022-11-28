cf12ccf49824da81eb788f62c404a48b5ab2903c
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2022-11-28 11:09:44 +0700

    set up for transaction page

---
+++ frontend/src/pages/Transaction/Transaction.tsx
@@ -0,0 +1,12 @@
+import {memo} from 'react'
+
+import styles from "./Transaction.module.css"
+
+function Transaction() {
+    return (
+        <>
+        </>
+    )
+}
+
+export default memo(Transaction)
