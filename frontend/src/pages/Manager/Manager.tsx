c19e9a64909ac2c8997fed3d6933a0de8fced4e7
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2022-11-30 15:12:28 +0700

    update 3h12

--- frontend/src/pages/Manager/Manager.tsx
+++ frontend/src/pages/Manager/Manager.tsx
@@ -1,4 +1,4 @@
-import {memo, useEffect} from 'react'
+import {memo, useEffect, useState} from 'react'
 
 import styles from "./Manager.module.css"
 
@@ -7,8 +7,18 @@ import Header from "../../components/User/Header/Header"
 import Brand from "../../components/Brand/Brand"
 
 import clsx from 'clsx'
+import ProductItem from '../../components/ProductItem/ProductItem'
+import { Button, Form } from 'react-bootstrap'
+import Warning from '../../components/Warning/Warning'
 
 function Manager() {
+    const [isWarning, setIsWarning] = useState(false)
+    function handleWarningShow() {
+        setIsWarning(true);
+    }
+    function handleWarningClose() {
+        setIsWarning(false);
+    }
     useEffect(() => {
         // scroll to top
         window.scrollTo(0, 0)
@@ -17,7 +27,24 @@ function Manager() {
         <>
             <div className={styles.container}>
                 <Header title="Store Manager" content="Manage your products"/>
-
+                <Warning show={isWarning} handleShow={handleWarningShow} handleClose={handleWarningClose} />
+                <div>
+                    <Form className="d-flex mt-3 mb-3">
+                        <Form.Control
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </div>
+                <div>
+                <ProductItem handleWarningShow={handleWarningShow}/>
+                <ProductItem handleWarningShow={handleWarningShow}/>
+                <ProductItem handleWarningShow={handleWarningShow}/>
+                <ProductItem handleWarningShow={handleWarningShow}/>
+                </div>
             </div>
         </>
     )
