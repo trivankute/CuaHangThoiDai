d7a8cdcfea1cf2ad06ccfb840f2af4982c322026
Ngo Gia Phong <ngogiaphong123@gmail.com>
2022-11-30 15:09:01 +0700

    Get user information by id

--- serverphp/model/user.php
+++ serverphp/model/user.php
@@ -37,5 +37,26 @@ class User {
             exit();
         }
     }
+    public function getUserInformationById($id) {
+        $sql = "SELECT * FROM user WHERE user_id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':id', $id);
+        try {
+            $stmt->execute();
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            $sql = "SELECT * FROM account WHERE user_id = :id";
+            $stmt = $this->conn->prepare($sql);
+            $stmt->bindParam(':id', $id);
+            $stmt->execute();
+            $account = $stmt->fetch(PDO::FETCH_ASSOC);
+            $result['account'] = $account;
+            // omit password
+            unset($result['account']['password']);
+            return $result;
+        } catch (PDOException $e) {
+            echo json_encode(['status' => 'error', 'data' => ['msg' => $e->getMessage()]]);
+            exit();
+        }
+    }
 }
 ?>
