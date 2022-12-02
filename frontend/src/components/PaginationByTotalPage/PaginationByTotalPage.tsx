28ba00d61e3fd001dbe83d5327020e2513a9b892
trivankute <van.trinhlu02113010@hcmut.edu.vn>
2022-12-02 15:39:59 +0700

    pagination system

--- frontend/src/components/PaginationByTotalPage/PaginationByTotalPage.tsx
+++ frontend/src/components/PaginationByTotalPage/PaginationByTotalPage.tsx
@@ -1,31 +1,113 @@
-import {memo} from 'react';
+import { memo, useEffect, useState } from 'react';
 import { Pagination } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { AlbumsStore } from '../../redux/selectors';
+import { getAlbumsAllPages } from '../../redux/slices/AlbumsSlice';
 
-function PaginationByTotalPage({totalPage}:{totalPage:any}) {
+function PaginationByTotalPage({ currPage, basicUrl }: { currPage: any, basicUrl: any }) {
+    const navigate = useNavigate();
+    const dispatch = useDispatch<any>();
+    const [pageTracking, setPageTracking] = useState<any>(false)
+    const albums = useSelector(AlbumsStore)
+    useEffect(() => {
+        function loadToArray(totalPage: any) {
+            if (totalPage) {
+                console.log(totalPage)
+                if (totalPage == 1)
+                    setPageTracking(["active"])
+                else if (totalPage == 2) {
+                    if (currPage == 1)
+                        setPageTracking(["active", "2"])
+                    else setPageTracking(["1", "active"])
+                }
+                else if (totalPage == 3) {
+                    if (currPage == 1)
+                        setPageTracking(["active", "2", "3"])
+                    else if (currPage == 2)
+                        setPageTracking(["1", "active", "3"])
+                    else setPageTracking(["1", "2", "active"])
+                }
+                else if (totalPage == 4) {
+                    if (currPage == 1)
+                        setPageTracking(["active", "2", "3", "4"])
+                    else if (currPage == 2)
+                        setPageTracking(["1", "active", "3", "4"])
+                    else if (currPage == 3)
+                        setPageTracking(["1", "2", "active", "4"])
+                    else setPageTracking(["1", "2", "3", "active"])
+                }
+                else {
+                    // 5 cases
+                    if (currPage == 1) {
+                        setPageTracking(["active", "2", "3", "...", "totalPage"])
+                    }
+                    else if (currPage == 2) {
+                        setPageTracking(["1", "active", "3", "4", "..."])
+                    }
+                    else if (currPage == totalPage - 1) {
+                        setPageTracking(["...", currPage - 2, currPage - 1, "active", totalPage])
+                    }
+                    else if (currPage == totalPage) {
+                        setPageTracking(["...", currPage - 3, currPage - 2, currPage - 1, "active"])
+                    }
+                    else {
+                        setPageTracking(["...", currPage - 1, "active", currPage + 1, "..."])
+                    }
+                }
+            }
+        }
+        dispatch(getAlbumsAllPages({ albumCount: 8 }))
+            .then((res: any) => {
+                loadToArray(res.payload.totalPage)
+            })
+    }, [currPage])
     return (
         <>
-        <Pagination>
-            {totalPage<=3?
-            <>
-            <Pagination.First />
-            <Pagination.Prev />
-            <Pagination.Next />
-            <Pagination.Last />
-            </>
-            :
-            <>
-            </>
-        }
-            <Pagination.First />
-            <Pagination.Prev />
-            <Pagination.Item active>{1}</Pagination.Item>
-            <Pagination.Item >{2}</Pagination.Item>
-            <Pagination.Item >{3}</Pagination.Item>
-            <Pagination.Ellipsis />
-            <Pagination.Item>{6}</Pagination.Item>
-            <Pagination.Next />
-            <Pagination.Last />
-        </Pagination>
+            {
+                pageTracking &&
+                <Pagination>
+                    {
+                        pageTracking.length >= 5 && currPage != 1
+                        &&
+                        <>
+                            <Pagination.First onClick={() => navigate(`${basicUrl}1`)} />
+                        </>
+                    }
+                    {
+                        pageTracking.length != 1 && currPage != 1
+                        &&
+                        <>
+                        <Pagination.Prev onClick={() => navigate(`${basicUrl}${parseInt(currPage) - 1}`)} />
+                        </>
+                    }
+                    {
+                        pageTracking.map((item: any, index: any) => {
+                            if (item == "active")
+                                return <Pagination.Item key={index} active>{currPage}</Pagination.Item>
+                            else if (item == "...")
+                                return <Pagination.Ellipsis key={index} />
+                            else return <Pagination.Item key={index} onClick={() => navigate(`${basicUrl}${item}`)}>{item}</Pagination.Item>
+                        })
+                    }
+                    {
+                        pageTracking.length !=1 && currPage != pageTracking.length
+                        &&
+                        <>
+                        <Pagination.Next onClick={() => navigate(`${basicUrl}${parseInt(currPage) + 1}`)} />
+                        </>
+                    }
+                    {
+                        pageTracking.length >= 5 && currPage != pageTracking.length
+                        &&
+                        <>
+                            <Pagination.Last onClick={() => navigate(`${basicUrl}${albums.data.totalPage}`)} />
+                        </>
+                    }
+
+
+                </Pagination>
+            }
         </>
     )
 }
--- frontend/src/pages/Albums/Albums.tsx
+++ frontend/src/pages/Albums/Albums.tsx
@@ -2,13 +2,13 @@ import { memo, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom'
 
 import styles from './Albums.module.css';
-import { Container, Row, Pagination } from "react-bootstrap"
+import { Container, Row } from "react-bootstrap"
 
 import AlbumCard from "../../components/Cards/AlbumCard/AlbumCard"
-import image from "./cd.png"
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllAlbumsByPageId } from '../../redux/slices/AlbumsSlice';
 import { AlbumsStore } from '../../redux/selectors';
+import PaginationByTotalPage from '../../components/PaginationByTotalPage/PaginationByTotalPage';
 
 function Albums() {
     const dispatch = useDispatch<any>()
@@ -23,14 +23,12 @@ function Albums() {
             return 'artist'
         else return false
     }
-    // scroll to top
     useEffect(() => {
         window.scrollTo(0, 0)
-    }, [])
-    useEffect(() => {
         if (pageId)
             dispatch(getAllAlbumsByPageId({ id: pageId, albumCount: 8 }))
                 .then((res: any) => {
+                    
                 })
     }, [pageId])
 
@@ -45,24 +43,14 @@ function Albums() {
                 {
                     albums.data &&
                     albums.data.map((album: any, index: any) => {
-                        console.log(album)
                         return (
                             <AlbumCard album_id={album.album_id} image={album.avatar} title={album.title} price={album.price} />
                         )
                     })}
             </Container>
             {/* add pagination */}
-            <Pagination className={styles.pagination}>
-                <Pagination.First />
-                <Pagination.Prev />
-                <Pagination.Item active>{1}</Pagination.Item>
-                <Pagination.Item >{2}</Pagination.Item>
-                <Pagination.Item >{3}</Pagination.Item>
-                <Pagination.Ellipsis />
-                <Pagination.Item>{6}</Pagination.Item>
-                <Pagination.Next />
-                <Pagination.Last />
-            </Pagination>
+            <PaginationByTotalPage currPage={pageId} basicUrl="/products/albums?page=" />
+
         </>
     )
 }
