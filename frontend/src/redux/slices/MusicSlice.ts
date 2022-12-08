e494dc922165b946818499de705b9b2ea9ab7575
Ngo Gia Phong <ngogiaphong123@gmail.com>
2022-12-08 23:00:30 +0700

    Carousel, Music, Footer Slice API called

---
+++ frontend/src/redux/slices/MusicSlice.ts
@@ -0,0 +1,145 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit"
+import axios from "axios";
+import { serverUrl } from "../../utils/config.utils";
+
+const MusicSlice = createSlice({
+    name:"MusicSlice",
+    initialState:{
+        loading:false,
+        data:false
+    },
+    reducers:{
+    },
+    extraReducers(builder) {
+        builder
+        .addCase(getAllMusic.pending, (state,action) => {
+            state.loading = true
+        })
+        .addCase(getAllMusic.fulfilled, (state,action) => {
+            state.loading = false
+            const {status, data} = action.payload
+            if(status==="success")
+            state.data = data
+        })
+        .addCase(createMusic.pending, (state,action) => {
+            state.loading = true
+        })
+        .addCase(createMusic.fulfilled, (state,action) => {
+            state.loading = false
+        })
+        .addCase(updateMusic.pending, (state,action) => {
+            state.loading = true
+        })
+        .addCase(updateMusic.fulfilled, (state,action) => {
+            state.loading = false
+        })
+        .addCase(deleteMusic.pending, (state,action) => {
+            state.loading = true
+        })
+        .addCase(deleteMusic.fulfilled, (state,action) => {
+            state.loading = false
+        })
+    }   
+})
+export const createCarousel = createAsyncThunk('createCarousel', async (input : any) => {
+    //{{host}}/api/carousels/create.php
+    try {
+        const {data} = await axios.post(`${serverUrl}/api/carousels/create.php`,input,{
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        if(data.status === 'success'){
+            return {status:"success", "msg":data.data.msg};
+        }
+        else {
+            return {status:"error","msg":data.data.msg};
+        }
+    }
+    catch(error : any) {
+        return {status:"error","msg":error.response.data.message};
+    }
+})
+export const createMusic = createAsyncThunk('createMusic', async (input : any) => {
+    try {
+        // {{host}}/api/musics/create.php
+        const {musicLink} = input;
+        const {data} = await axios.post(`${serverUrl}/api/musics/create.php`,{
+            musicLink
+        },{
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        if(data.status === 'success'){
+            return {status:"success", "msg":data.data.msg};
+        }
+        else {
+            return {status:"error","msg":data.data.msg};
+        }
+    }
+    catch(error : any) {
+        return {status:"error","msg":error.response.data.message};
+    }
+})
+export const getAllMusic = createAsyncThunk('getAllMusic', async () => {
+    try {
+        //{{host}}/api/musics/getAll.php
+        const {data} = await axios.get(`${serverUrl}/api/musics/getAll.php`,{
+            headers: {
+            }
+        });
+        if(data.status === 'success'){
+            return {status:"success", "data":data.data};
+        }
+        else {
+            return {status:"error","msg":data.data.msg};
+        }
+    }
+    catch(error : any) {
+        return {status:"error","msg":error.response.data.message};
+    }
+})
+export const updateMusic = createAsyncThunk('updateMusic', async (input : any) => {
+    try {
+        // {{host}}/api/musics/update.php?id=1
+        const {id, musicLink} = input;
+        const {data} = await axios.put(`${serverUrl}/api/musics/update.php?id=${id}`,{
+            musicLink
+        },{
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        if(data.status === 'success'){
+            return {status:"success", "msg":data.data.msg};
+        }
+        else {
+            return {status:"error","msg":data.data.msg};
+        }
+    }
+    catch(error : any) {
+        return {status:"error","msg":error.response.data.message};
+    }
+})
+export const deleteMusic = createAsyncThunk('deleteMusic', async (input : any) => {
+    try {
+        // {{host}}/api/musics/delete.php?id=1
+        const {id} = input;
+        const {data} = await axios.delete(`${serverUrl}/api/musics/delete.php?id=${id}`,{
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+        if(data.status === 'success'){
+            return {status:"success", "msg":data.data.msg};
+        }
+        else {
+            return {status:"error","msg":data.data.msg};
+        }
+    }
+    catch (error : any) {
+        return {status:"error","msg":error.response.data.message};
+    }
+})
+export default MusicSlice;
